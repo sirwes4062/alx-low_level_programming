9c9dca0c19e1215f5e4b7e34d2ab92467e1cbd2d
sirwes4062 <williamseneojo@gmail.com>
2022-09-09 14:29:58 -0700

    task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the project
+ * description - Write a C program that prints exactly with proper grammar
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
