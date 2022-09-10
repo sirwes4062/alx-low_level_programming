d43ea0662503bb6bd79f9019821584299e8bc1fd
sirwes4062 <williamseneojo@gmail.com>
2022-09-09 22:09:44 -0700

    task 7

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the program
+ * description - Write a C program that prints the size of various types on the computer it is compiled and run on.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(d));
+	printf("size of a int: %lu byte(s)\n", (unsigned long) sizeof(a));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d);
+	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+	return (0);
+}
+
