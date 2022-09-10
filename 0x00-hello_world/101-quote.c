39afcb05b35d3899210b47d4fab768a30aa84979
sirwes4062 <williamseneojo@gmail.com>
2022-09-10 03:58:49 -0700

    new task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - this is the entry point of the program
+ * description -  prints exactly and that piece of art is useful
+ * Return: 0
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"59);
+return (1);
+}
