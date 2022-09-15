26193f35d6026280b048772de05e976f314475b4
sirwes4062 <williamseneojo@gmail.com>
2022-09-14 23:00:34 -0700

    new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,8 +14,8 @@ void jack_bauer(void)
 	{
 		while (minutes <= 59)
 		{
-			mins_remainder = minute % 10;
-			hours_remiander = hours % 10;
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 10;
 			_putchar(hours / 10 + '0');
 
 			_putchar(hours_remainder + 0);
