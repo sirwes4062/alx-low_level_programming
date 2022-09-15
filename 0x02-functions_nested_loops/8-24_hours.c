96f14b345d5f3e47905e3a712b57d864493af81b
sirwes4062 <williamseneojo@gmail.com>
2022-09-14 22:47:18 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - func that prints evert minute of the day of jack Bauer
+ * Return: always 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			mins_remainder = minute % 10;
+			hours_remiander = hours % 10;
+			_putchar(hours / 10 + '0');
+
+			_putchar(hours_remainder + 0);
+
+			_putchar(':');
+
+			_putchar(minutes / 10 + '0');
+
+			_putchar(mins_remainder + '0');
+
+			minutes++;
+			
+			_putchar('\n');
+
+		}
+
+		hours++;
+
+		minutes = 0;
+
+	}
+}   
